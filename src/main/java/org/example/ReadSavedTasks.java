1a7e1d47b38be8ab5180ccd569a984cafc7134d8
lukoc1 <lukoc@visa.com>
2026-07-03 09:53:16 +0200

    Reading previous tasks from .csv and saving to multiarray "tasks"

---
+++ src/main/java/org/example/ReadSavedTasks.java
@@ -0,0 +1,37 @@
+package org.example;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class ReadSavedTasks {
+    public static String[][] readSavedTasks() {
+        var fileName = "tasks.csv";
+        try (var scan = new Scanner(new File(fileName));
+             var scan2 = new Scanner(new File(fileName))){
+
+            var rowsNum = 0;
+            while (scan.hasNextLine()) {
+                scan.nextLine();
+                rowsNum++;
+            }
+
+            String[][] tasks = new String[rowsNum][3];
+
+            int row = 0;
+            while(scan2.hasNextLine()) {
+                var line = scan2.nextLine();
+                var parts = line.split("," + "\\s+");
+                tasks[row][0] = parts[0];
+                tasks[row][1] = parts[1];
+                tasks[row][2] = parts[2];
+                row++;
+            }
+            return (tasks);
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Nie udało się odczytać pliku: " + fileName);
+        }
+        return (new String[0][0]);
+    }
+}
--- src/main/java/org/example/TaskManager.java
+++ src/main/java/org/example/TaskManager.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class TaskManager {
     public static void main(String[] args) {
-//        String[][] tasks = ReadSavedTasks.readSavedTasks();
+        String[][] tasks = ReadSavedTasks.readSavedTasks();
         UI.displayMenu();
     }
 }
